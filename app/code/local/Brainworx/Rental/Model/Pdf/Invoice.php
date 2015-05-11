382afd6d97c77a96f6e5241cbcda68897fe8ae13
Stijn <Stijn.heylen@brainworx.be>
2015-05-11 20:54:03 +0200

    Fix display billing address invoice Fix invoice footer address and email line Added translations Fix shorten new order email by 1 rule

--- app/code/local/Brainworx/Rental/Model/Pdf/Invoice.php
+++ app/code/local/Brainworx/Rental/Model/Pdf/Invoice.php
@@ -36,7 +36,7 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 			);
 			/* Add invoice date */
 			$this->insertInvoiceDate($page, 
-					Mage::helper('sales')->__('Invoice Date ') .
+					Mage::helper('sales')->__('Invoice Date: ') .
 					Mage::helper('core')->formatDate($invoice->getCreatedAt(), 'medium', false));
 								/* Add table */
 			$this->_drawHeader($page);
@@ -129,15 +129,16 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 		$this->_setFontRegular($page, 10);
 	
 		$startX = 50;
-		$startY = 100;//80
-		$columnWidth = 125;
+		$startY = 80;
+		$columnWidth = 165; //120
 		$lineY = 12;
 	
 		$name = Mage::getStoreConfig('general/store_information/name');
 		// Footer title
 		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.25));//0.25
 	
-		$text = "We danken je voor het vertrouwen in ".$name.".";
+		//$text = "We danken je voor het vertrouwen in ".$name.".";
+		$text = Mage::helper('sales')->__('Thank you for trusting ').$name."."; 
 	
 		$page->drawText($text, $startX, $startY, 'UTF-8');
 	
@@ -203,5 +204,242 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 // 		$y = $startY;
 	
 // 		$page->drawText($store->getFrontendName(), $x, $y, 'UTF-8');
+	}
+	protected function insertOrder(&$page, $obj, $putOrderId = true)
+	{
+		if ($obj instanceof Mage_Sales_Model_Order) {
+			$shipment = null;
+			$order = $obj;
+		} elseif ($obj instanceof Mage_Sales_Model_Order_Shipment) {
+			$shipment = $obj;
+			$order = $shipment->getOrder();
+		}
+	
+		$this->y = $this->y ? $this->y : 815;
+		$top = $this->y;
+	
+		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.45));
+		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.45));
+		$page->drawRectangle(25, $top, 570, $top - 55);
+		$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+		$this->setDocHeaderCoordinates(array(25, $top, 570, $top - 55));
+		$this->_setFontRegular($page, 10);
+	
+		if ($putOrderId) {
+			$page->drawText(
+					Mage::helper('sales')->__('Order # ') . $order->getRealOrderId(), 35, ($top -= 30), 'UTF-8'
+			);
+		}
+		//SHE move order date to right side
+		$page->drawText(
+				Mage::helper('sales')->__('Order Date: ') . Mage::helper('core')->formatDate(
+						$order->getCreatedAtStoreDate(), 'medium', false
+				),
+				450,//35
+				($top),//15
+				'UTF-8'
+		);
+	
+		$top -= 10;
+		$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+		$page->setLineWidth(0.5);
+		$page->drawRectangle(25, $top, 275, ($top - 25));
+		$page->drawRectangle(275, $top, 570, ($top - 25));
+	
+		/* Calculate blocks info */
+	
+		/* Billing Address */
+		$billingAddress = $this->_formatAddress($order->getBillingAddress()->format('pdf'));
+	
+		/* Payment */
+		$paymentInfo = Mage::helper('payment')->getInfoBlock($order->getPayment())
+		->setIsSecureMode(true)
+		->toPdf();
+		$paymentInfo = htmlspecialchars_decode($paymentInfo, ENT_QUOTES);
+		$payment = explode('{{pdf_row_separator}}', $paymentInfo);
+		foreach ($payment as $key=>$value){
+			if (strip_tags(trim($value)) == '') {
+				unset($payment[$key]);
+			}
+		}
+		reset($payment);
+	
+		/* Shipping Address and Method */
+		if (!$order->getIsVirtual()) {
+			/* Shipping Address */
+			$shippingAddress = $this->_formatAddress($order->getShippingAddress()->format('pdf'));
+			$shippingMethod  = $order->getShippingDescription();
+		}
+	
+		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+		$this->_setFontBold($page, 12);
+		//SHE override position of billingaddress to rigth
+		$page->drawText(Mage::helper('sales')->__('Sold to:'), 285, ($top - 15), 'UTF-8');
+	
+		if (!$order->getIsVirtual()) {
+			$page->drawText(Mage::helper('sales')->__('Ship to:'), 35, ($top - 15), 'UTF-8');
+		} else {
+			$page->drawText(Mage::helper('sales')->__('Payment Method:'), 285, ($top - 15), 'UTF-8');
+		}
+	
+		$addressesHeight = $this->_calcAddressHeight($billingAddress);
+		if (isset($shippingAddress)) {
+			$addressesHeight = max($addressesHeight, $this->_calcAddressHeight($shippingAddress));
+		}
+	
+		$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+		$page->drawRectangle(25, ($top - 25), 570, $top - 33 - $addressesHeight);
+		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+		$this->_setFontRegular($page, 10);
+		$this->y = $top - 40;
+		$addressesStartY = $this->y;
+	
+		foreach ($billingAddress as $value){
+			if ($value !== '') {
+				$text = array();
+				foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+					$text[] = $_value;
+				}
+				foreach ($text as $part) {
+					$page->drawText(strip_tags(ltrim($part)), 35, $this->y, 'UTF-8');
+					$this->y -= 15;
+				}
+			}
+		}
+	
+		$addressesEndY = $this->y;
+	
+		if (!$order->getIsVirtual()) {
+			$this->y = $addressesStartY;
+			foreach ($shippingAddress as $value){
+				if ($value!=='') {
+					$text = array();
+					foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+						$text[] = $_value;
+					}
+					foreach ($text as $part) {
+						$page->drawText(strip_tags(ltrim($part)), 285, $this->y, 'UTF-8');
+						$this->y -= 15;
+					}
+				}
+			}
+	
+			$addressesEndY = min($addressesEndY, $this->y);
+			$this->y = $addressesEndY;
+	
+			$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+			$page->setLineWidth(0.5);
+			$page->drawRectangle(25, $this->y, 275, $this->y-25);
+			$page->drawRectangle(275, $this->y, 570, $this->y-25);
+	
+			$this->y -= 15;
+			$this->_setFontBold($page, 12);
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+			$page->drawText(Mage::helper('sales')->__('Payment Method'), 35, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('Shipping Method:'), 285, $this->y , 'UTF-8');
+	
+			$this->y -=10;
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+	
+			$this->_setFontRegular($page, 10);
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+	
+			$paymentLeft = 35;
+			$yPayments   = $this->y - 15;
+		}
+		else {
+			$yPayments   = $addressesStartY;
+			$paymentLeft = 285;
+		}
+	
+		foreach ($payment as $value){
+			if (trim($value) != '') {
+				//Printing "Payment Method" lines
+				$value = preg_replace('/<br[^>]*>/i', "\n", $value);
+				foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+					$page->drawText(strip_tags(trim($_value)), $paymentLeft, $yPayments, 'UTF-8');
+					$yPayments -= 15;
+				}
+			}
+		}
+	
+		if ($order->getIsVirtual()) {
+			// replacement of Shipments-Payments rectangle block
+			$yPayments = min($addressesEndY, $yPayments);
+			$page->drawLine(25,  ($top - 25), 25,  $yPayments);
+			$page->drawLine(570, ($top - 25), 570, $yPayments);
+			$page->drawLine(25,  $yPayments,  570, $yPayments);
+	
+			$this->y = $yPayments - 15;
+		} else {
+			$topMargin    = 15;
+			$methodStartY = $this->y;
+			$this->y     -= 15;
+	
+			foreach (Mage::helper('core/string')->str_split($shippingMethod, 45, true, true) as $_value) {
+				$page->drawText(strip_tags(trim($_value)), 285, $this->y, 'UTF-8');
+				$this->y -= 15;
+			}
+	
+			$yShipments = $this->y;
+			$totalShippingChargesText = "(" . Mage::helper('sales')->__('Total Shipping Charges') . " "
+					. $order->formatPriceTxt($order->getShippingAmount()) . ")";
+	
+			$page->drawText($totalShippingChargesText, 285, $yShipments - $topMargin, 'UTF-8');
+			$yShipments -= $topMargin + 10;
+	
+			$tracks = array();
+			if ($shipment) {
+				$tracks = $shipment->getAllTracks();
+			}
+			if (count($tracks)) {
+				$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+				$page->setLineWidth(0.5);
+				$page->drawRectangle(285, $yShipments, 510, $yShipments - 10);
+				$page->drawLine(400, $yShipments, 400, $yShipments - 10);
+				//$page->drawLine(510, $yShipments, 510, $yShipments - 10);
+	
+				$this->_setFontRegular($page, 9);
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+				//$page->drawText(Mage::helper('sales')->__('Carrier'), 290, $yShipments - 7 , 'UTF-8');
+				$page->drawText(Mage::helper('sales')->__('Title'), 290, $yShipments - 7, 'UTF-8');
+				$page->drawText(Mage::helper('sales')->__('Number'), 410, $yShipments - 7, 'UTF-8');
+	
+				$yShipments -= 20;
+				$this->_setFontRegular($page, 8);
+				foreach ($tracks as $track) {
+	
+					$CarrierCode = $track->getCarrierCode();
+					if ($CarrierCode != 'custom') {
+						$carrier = Mage::getSingleton('shipping/config')->getCarrierInstance($CarrierCode);
+						$carrierTitle = $carrier->getConfigData('title');
+					} else {
+						$carrierTitle = Mage::helper('sales')->__('Custom Value');
+					}
+	
+					//$truncatedCarrierTitle = substr($carrierTitle, 0, 35) . (strlen($carrierTitle) > 35 ? '...' : '');
+					$maxTitleLen = 45;
+					$endOfTitle = strlen($track->getTitle()) > $maxTitleLen ? '...' : '';
+					$truncatedTitle = substr($track->getTitle(), 0, $maxTitleLen) . $endOfTitle;
+					//$page->drawText($truncatedCarrierTitle, 285, $yShipments , 'UTF-8');
+					$page->drawText($truncatedTitle, 292, $yShipments , 'UTF-8');
+					$page->drawText($track->getNumber(), 410, $yShipments , 'UTF-8');
+					$yShipments -= $topMargin - 5;
+				}
+			} else {
+				$yShipments -= $topMargin - 5;
+			}
+	
+			$currentY = min($yPayments, $yShipments);
+	
+			// replacement of Shipments-Payments rectangle block
+			$page->drawLine(25,  $methodStartY, 25,  $currentY); //left
+			$page->drawLine(25,  $currentY,     570, $currentY); //bottom
+			$page->drawLine(570, $currentY,     570, $methodStartY); //right
+	
+			$this->y = $currentY;
+			$this->y -= 15;
+		}
 	}
 }
