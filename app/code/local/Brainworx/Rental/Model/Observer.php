45e65bdd08a7b5042cb0496717aabf06ff8c9aa5
Stijn <Stijn.heylen@brainworx.be>
2015-10-23 15:15:20 +0200

    Fase 2 - oplevering emails for supplier invoiced items + need to create product attribute supplier_order_email first as viible in frontend

--- app/code/local/Brainworx/Rental/Model/Observer.php
+++ app/code/local/Brainworx/Rental/Model/Observer.php
@@ -8,6 +8,12 @@
 class Brainworx_Rental_Model_Observer
 {
 	/**
+	 * XML configuration paths
+	 */
+	const XML_PATH_EMAIL_IDENTITY               = 'sales_email/order/identity';
+	const XML_PATH_EMAIL_COPY_TO                = 'sales_email/order/copy_to';
+	const XML_PATH_EMAIL_COPY_METHOD            = 'sales_email/order/copy_method';
+	/**
 	 * Observer method configured for sales_order_place_after
 	 * 
 	 * Save new rental items to be invoiced monthly.
@@ -18,141 +24,228 @@ class Brainworx_Rental_Model_Observer
 	public function checkNewRental(Varien_Event_Observer $observer)
 	{
 		try{
-		Mage::Log('sales_order_place_after');
-		// Retrieve the product being updated from the event observer
-		$order = $observer->getEvent()->getOrder();
-		// array_keys($order->getData() prints teh data keys in the order - can later be used to getData($key);
-		
-		if($order->getCustomerId() === NULL){
-			Mage::Log("Guest: " .$order->getBillingAddress()->getLastname());
-		}
-		else {
-			Mage::Log("Known customer: " .$order->getCustomerId());
-		}
-		//Check tax record
-		
-		$items=$order->getAllVisibleItems();
-		Mage::Log("nr items : " . count($items));
-		$count = 0;
-		$process = 0;
-		$rentaltosave = false;
-		foreach ($items as  $item)
-		{
+			
+			// Retrieve the product being updated from the event observer
+			$order = $observer->getEvent()->getOrder();
+			Mage::log("order ".$order->getEntityId());
+			// array_keys($order->getData() prints teh data keys in the order - can later be used to getData($key);
+			
+			//Check tax record		
+			$items=$order->getAllVisibleItems();
+			$count = 0;
+			$process = 0;
 			$rentaltosave = false;
-			if(!empty($item->getRentalitem())&&$item->getRentalitem() == true){
-				$rentaltosave = true;
-			}else{
-				//TODO remove this check as not needed
-				foreach($item->getProduct()->getCategoryIds() as $cat){
-					if($cat ==
-					  Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('CAT_RENT')->getValue('text')){
-						$rentaltosave = true;
-						Mage::Log("checknewrental found rental cat for line without rentalitem identifier:" .$cat);							
-						break;
+			$suppliersToEmail = array();
+			foreach ($items as  $item)
+			{
+				if(!empty($item->getSupplierneworderemail())
+						&&!in_array($item->getSupplierneworderemail(),$suppliersToEmail)){
+					$suppliersToEmail[]=$item->getSupplierneworderemail();
+				}
+				//check $item->getProduct()->getSupplierOrderEmail() instead
+// 				$semail = $item->getProduct()->getSupplierOrderEmail();
+// 				if(!empty($semail)
+// 						&&!in_array($email,$suppliersToEmail)){
+// 					$suppliersToEmail[]=$semail;
+// 				}
+				$rentaltosave = false;
+				if(!empty($item->getRentalitem())&&$item->getRentalitem() == true){
+					$rentaltosave = true;
+				}else{
+					//TODO remove this check as not needed
+					foreach($item->getProduct()->getCategoryIds() as $cat){
+						if($cat ==
+						  Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('CAT_RENT')->getValue('text')){
+							$rentaltosave = true;						
+							break;
+						}
 					}
 				}
-			}
-			if($rentaltosave){
-				$count++;
-				//saving new rental line to be invoiced monthly
-				try{
-					$newrentalitem = Mage::getModel('rental/rentedItem');
-				
-					$newrentalitem->setData('orig_order_id',$order->getEntityId());
-					$newrentalitem->setData('order_item_id',$item->getItemId());
-					$newrentalitem->setData('quantity',$item->getQtyOrdered());// nr of items - not days
-					$newrentalitem->setStartDt(date("Y-m-d"));
-					Mage::Log('date:'.Mage::getModel('core/date')->date('Y-m-d'));
-				
-					$newrentalitem->Save();
-				
-					Mage::Log("new rental (product:".$item->getName()." - Q ".$item->getQtyOrdered()." after saved for order " . $order->getEntityId());
+				if($rentaltosave){
+					$count++;
+					//saving new rental line to be invoiced monthly
+					try{
+						$newrentalitem = Mage::getModel('rental/rentedItem');
+					
+						$newrentalitem->setData('orig_order_id',$order->getEntityId());
+						$newrentalitem->setData('order_item_id',$item->getItemId());
+						$newrentalitem->setData('quantity',$item->getQtyOrdered());// nr of items - not days
+						$newrentalitem->setStartDt(date("Y-m-d"));
+						Mage::Log('date:'.Mage::getModel('core/date')->date('Y-m-d'));
+					
+						$newrentalitem->Save();
 						
-					//Check/add sale_tax_item record
-					if(0==count(Mage::getModel('tax/sales_order_tax_item')->getCollection()->addFieldToFilter(
-							array('item_id'),
-							array(array('eq'=> $item->getItemId()))))){
-						$taxCollection = Mage::getModel('tax/calculation')->getCollection();
-						// add joined data to the collection
-						$taxCollection->getSelect()->join(
-								array('rate' => 'tax_calculation_rate'),
-								'main_table.tax_calculation_rate_id = rate.tax_calculation_rate_id',
-								array('code','rate')
-						)->join(
-								array('rule' => 'tax_calculation_rule'),
-								'main_table.tax_calculation_rule_id = rule.tax_calculation_rule_id',
-								array('priority','position')
-						);
-						$custTaxClassID = Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('CUST_TAX_ID')->getValue('text');
-						$taxCollection->addFieldToFilter(
-								array('customer_tax_class_id'),
-								array(
-										array('eq'=>$custTaxClassID)) //TODO update customer tax class from real customer
-						)->addFieldToFilter(
-								array('product_tax_class_id'),
-								array(
-										array('eq'=>$item->getProduct()->getData('tax_class_id')))
-						);
-								foreach ( $taxCollection as $taxR ) {
-									$code = $taxR->getData("code");
-									//Check add sale_tax
-									$saleTaxes = Mage::getModel('tax/sales_order_tax')->getCollection()
-									->addFieldToFilter(
-											array('order_id'),
-											array(array('eq'=> $order->getEntityId())))
-											->addFieldToFilter(
-													array('code'),
-													array(array('eq'=> $code)));
-									if(0==count($saleTaxes)){
-										$data = array(
-												'order_id'          => $order->getId(),
-												'code'              => $code,
-												'title'             => $code, // here you could check tax_calculation_rate_title for overloaded title-if multiple stores exist
-												'hidden'            => 0,
-												'percent'           => $taxR->getData('rate'),
-												'priority'          => $taxR->getData('priority'),
-												'position'          => $taxR->getData('position'),
-												'amount'            => 0,
-												'base_amount'       => 0,
-												'process'           => $process,
-												'base_real_amount'  => 0,
-										);
-										$saleTax = Mage::getModel('tax/sales_order_tax')->setData($data)->save();
-										//process = used to sort tax rates per order
-										$process++;
-									}else{
-										foreach($saleTaxes as $st){
-											$saleTax = $st;
-											break;
+						//Check/add sale_tax_item record
+						if(0==count(Mage::getModel('tax/sales_order_tax_item')->getCollection()->addFieldToFilter(
+								array('item_id'),
+								array(array('eq'=> $item->getItemId()))))){
+							$taxCollection = Mage::getModel('tax/calculation')->getCollection();
+							// add joined data to the collection
+							$taxCollection->getSelect()->join(
+									array('rate' => 'tax_calculation_rate'),
+									'main_table.tax_calculation_rate_id = rate.tax_calculation_rate_id',
+									array('code','rate')
+							)->join(
+									array('rule' => 'tax_calculation_rule'),
+									'main_table.tax_calculation_rule_id = rule.tax_calculation_rule_id',
+									array('priority','position')
+							);
+							$custTaxClassID = Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('CUST_TAX_ID')->getValue('text');
+							$taxCollection->addFieldToFilter(
+									array('customer_tax_class_id'),
+									array(
+											array('eq'=>$custTaxClassID)) //TODO update customer tax class from real customer
+							)->addFieldToFilter(
+									array('product_tax_class_id'),
+									array(
+											array('eq'=>$item->getProduct()->getData('tax_class_id')))
+							);
+									foreach ( $taxCollection as $taxR ) {
+										$code = $taxR->getData("code");
+										//Check add sale_tax
+										$saleTaxes = Mage::getModel('tax/sales_order_tax')->getCollection()
+										->addFieldToFilter(
+												array('order_id'),
+												array(array('eq'=> $order->getEntityId())))
+												->addFieldToFilter(
+														array('code'),
+														array(array('eq'=> $code)));
+										if(0==count($saleTaxes)){
+											$data = array(
+													'order_id'          => $order->getId(),
+													'code'              => $code,
+													'title'             => $code, // here you could check tax_calculation_rate_title for overloaded title-if multiple stores exist
+													'hidden'            => 0,
+													'percent'           => $taxR->getData('rate'),
+													'priority'          => $taxR->getData('priority'),
+													'position'          => $taxR->getData('position'),
+													'amount'            => 0,
+													'base_amount'       => 0,
+													'process'           => $process,
+													'base_real_amount'  => 0,
+											);
+											$saleTax = Mage::getModel('tax/sales_order_tax')->setData($data)->save();
+											//process = used to sort tax rates per order
+											$process++;
+										}else{
+											foreach($saleTaxes as $st){
+												$saleTax = $st;
+												break;
+											}
 										}
 									}
-								}
-				
-								//load data for sale tax item
-								$data = array(
-										'item_id'       => $item->getItemId(),
-										'tax_id'        => $saleTax->getTaxId(),
-										'tax_percent'   => $saleTax->getPercent()
-								);
-								//save the sale tax item
-								Mage::getModel('tax/sales_order_tax_item')->setData($data)->save();
+					
+									//load data for sale tax item
+									$data = array(
+											'item_id'       => $item->getItemId(),
+											'tax_id'        => $saleTax->getTaxId(),
+											'tax_percent'   => $saleTax->getPercent()
+									);
+									//save the sale tax item
+									Mage::getModel('tax/sales_order_tax_item')->setData($data)->save();
+						}
+					}catch(Exception $e){
+						Mage::log($e->getMessage());
+						//set error message in session
+						Mage::getSingleton('core/session')->addError('Sorry, er gebeurde een fout tijdens het wegschrijven van je verhuur order.');
+						die;
 					}
-				}catch(Exception $e){
-					Mage::log($e->getMessage());
-					//set error message in session
-					Mage::getSingleton('core/session')->addError('Sorry, er gebeurde een fout tijdens het wegschrijven van je verhuur order.');
-					die;
 				}
+				 
 			}
-			 
-		}
-		if($count > 0){
-			//add comment which will be sent to the customer
-			//TODO add translation Mage::helper('sales')->__('Invoice Date ')
-			$order->addStatusToHistory($order->getStatus(), 'Verhuurartikels worden aangerekend per dag en maandelijks gefactureerd.', true);
-			$order->save();
-		}
-		Mage::Log("Sale done: nr rental items : " . $count);
+			if($count > 0){
+				//add comment which will be sent to the customer
+				//TODO add translation Mage::helper('sales')->__('Invoice Date ')
+				$order->addStatusToHistory($order->getStatus(), 'Verhuurartikels worden aangerekend per dag en maandelijks gefactureerd.', true);
+				$order->save();
+			}
+			Mage::log('Need to send '.count($suppliersToEmail).' supplier emails.');
+			if(!empty($suppliersToEmail)){
+				// This is the template name from your etc/config.xml
+				$template_id = 'supplier_order_new';
+				$storeId = Mage::app()->getStore()->getId();
+				
+				foreach($suppliersToEmail as $email){
+					//send new order email to supplier
+					
+					// Who were sending to...
+					$email_to = $email;				
+					// Load our template by template_id
+					$email_template  = Mage::getModel('core/email_template')->loadDefault($template_id);
+					
+					// Here is where we can define custom variables to go in our email template!
+					$email_template_variables = array(
+							 'order'        => $order,
+							 'supplieremail'=> $email
+							// Other variables for our email template.
+					);
+					
+					// I'm using the Store Name as sender name here.
+					$sender_name = Mage::getStoreConfig(Mage_Core_Model_Store::XML_PATH_STORE_STORE_NAME);
+					// I'm using the general store contact here as the sender email.
+					$sender_email = Mage::getStoreConfig('trans_email/ident_general/email');
+					$email_template->setSenderName($sender_name);
+					$email_template->setSenderEmail($sender_email);
+					$email_template->addBcc($sender_email);
+					
+					//Send the email!
+					$email_template->send($email_to, Mage::helper('rental')->__('Supplier'), $email_template_variables);
+					
+					//via queu
+					
+					// Get the destination email addresses to send copies to
+					//$copyTo = array(Mage::getStoreConfig(self::XML_PATH_EMAIL_IDENTITY, $storeId));
+// 					$copyTo = array(Mage::getStoreConfig('trans_email/ident_general/email'));
+// 					$copyMethod = Mage::getStoreConfig(self::XML_PATH_EMAIL_COPY_METHOD, $storeId);
+										
+// 					/** @var $mailer Mage_Core_Model_Email_Template_Mailer */
+// 					$mailer = Mage::getModel('core/email_template_mailer');
+// 					/** @var $emailInfo Mage_Core_Model_Email_Info */
+// 					$emailInfo = Mage::getModel('core/email_info');
+// 					$emailInfo->addTo($email, Mage::helper('rental')->__('Supplier'));
+// 					if ($copyTo && $copyMethod == 'bcc') {
+// 						// Add bcc to customer email
+// 						foreach ($copyTo as $emailc) {
+// 							$emailInfo->addBcc($emailc);
+// 							Mage::log('copy email to '.$emailc);
+// 						}
+// 					}
+// 					$mailer->addEmailInfo($emailInfo);
+					
+					
+// 					// Email copies are sent as separated emails if their copy method is
+// 					// 'copy' or a customer should not be notified
+// 					if ($copyTo && ($copyMethod == 'copy')) {
+// 						foreach ($copyTo as $emailc) {
+// 							$emailInfo = Mage::getModel('core/email_info');
+// 							$emailInfo->addTo($emailc);
+// 							$mailer->addEmailInfo($emailInfo);
+// 							Mage::log('copy email to '.$emailc);
+// 						}
+// 					}
+					
+// 					// Set all required params and send emails
+// 					$mailer->setSender(Mage::getStoreConfig('trans_email/ident_general/email'));
+// 	       			$mailer->setStoreId($storeId);
+// 					$mailer->setTemplateId($template_id);
+// 					$mailer->setTemplateParams(array(
+// 							 'order'        => $order,
+// 							 'supplieremail'=> $email
+// 					)
+// 					);			
+// 					/** @var $emailQueue Mage_Core_Model_Email_Queue */
+// 					$emailQueue = Mage::getModel('core/email_queue');
+// 					$emailQueue->setEntityId($order->getEntityId())
+// 					->setEntityType('order')
+// 					->setEventType('new_supplier_order')
+// 					->setIsForceCheck(false);
+
+// 					$mailer->setQueue($emailQueue);
+// 					$mailer->send();
+					Mage::log('Email to supplier '.$email.' sent. order '.$order->getEntityId());
+				}
+			}		
+			Mage::Log("Sale done: nr rental items : " . $count);
 		
 		}catch(Exception $e){
 			Mage::log($e->getMessage());
@@ -164,7 +257,10 @@ class Brainworx_Rental_Model_Observer
 	}
 	/**
 	 * Observer method configured for sales_quote_product_add_after
-	 * After adding a rental item the price will be set to 0 as rental items will be monthly invoiced after the first month.
+	 * After adding a rental item or item invoiced by the supplier
+	 * the price will be set to 0 as rental items will be monthly invoiced after the first month.
+	 * There will also be an indicator on the orderitem for rentals (rentalitem)
+	 *  and/or invoiced by supplier (Supplierinvoice).
 	 */
 	public function addDiscountToRental(Varien_Event_Observer $observer)
 	{
@@ -191,6 +287,16 @@ class Brainworx_Rental_Model_Observer
 				foreach($item->getProduct()->getCategoryIds() as $cat){						
 					foreach ($catssuppplinvl as $scat){
 						if($cat == $scat){
+							//set supplier email
+							if(!empty($item->getProduct()->getSupplierOrderEmail())){
+								Mage::log('Product '.$item->getProduct()->getSku().' supplier email '.$item->getProduct()->getSupplierOrderEmail());
+								$item->setSupplierneworderemail($item->getProduct()->getSupplierOrderEmail());
+							}else{
+								Mage::log('Article invoiced by supplier but no email '.$item->getProduct()->getSku());
+								$item->setSupplierneworderemail(Mage::getStoreConfig('trans_email/ident_general/email'));
+							}
+							Mage::log('Item with Product '.$item->getProduct()->getSku().' supplier email '.$item->getSupplierneworderemail());
+							
 							$sinotice = 1;
 							break;
 						}
@@ -201,7 +307,6 @@ class Brainworx_Rental_Model_Observer
 				$item->setCustomPrice(0);
 				$item->setOriginalCustomPrice(0);
 				$item->getProduct()->setIsSuperMode(true);
-				Mage::Log("Custom price set for rental ".$rnotice." or supplier invoice ".$sinotice." " . $item->getProduct()->getName() . " -" . $item->getProduct()->getSku());
 			}
 			if($rnotice > 0){
 				Mage::getSingleton('core/session')->addNotice(Mage::helper('sales')->__('The price of your rental article has been set to 0, you will pay for this article within 10 days after receiving the monthly invoice.'));
---
+++ app/code/local/Brainworx/Rental/sql/rental_setup/upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,18 @@
+<?php
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
+/**
+ * Add 'custom_attribute' attribute for entities
+*/
+$entities = array(
+		'quote_item',
+		'order_item'
+);
+$options = array(
+		'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+		'visible'  => true,
+		'required' => false,
+);
+foreach ($entities as $entity) {
+	$installer->addAttribute($entity, 'supplierneworderemail', $options);
+}
+$installer->endSetup();
