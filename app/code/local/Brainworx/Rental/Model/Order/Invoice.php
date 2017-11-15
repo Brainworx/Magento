723184291ca336c81fef32c6c285a077d4d1e9d2
Stijn <stijn.heylen@mobyus.be>
2017-11-15 01:43:02 +0100

    Ticket 297: commit 2 - incasso

--- app/code/local/Brainworx/Rental/Model/Order/Invoice.php
+++ app/code/local/Brainworx/Rental/Model/Order/Invoice.php
@@ -1,6 +1,7 @@
 <?php
 class Brainworx_Rental_Model_Order_Invoice extends Mage_Sales_Model_Order_Invoice
 {
+	const STATE_INCASSO      = 11;
 	/*
 	 * Overriding sendEmail in order to be able to force the sending of emails 
 	 */
@@ -115,12 +116,33 @@ class Brainworx_Rental_Model_Order_Invoice extends Mage_Sales_Model_Order_Invoic
 		if (is_null(self::$_states)) {
 			self::$_states = array(
 					self::STATE_OPEN       => Mage::helper('sales')->__('Pending'),
-					11       => Mage::helper('sales')->__('Incasso'),
+					self::STATE_INCASSO       => Mage::helper('sales')->__('Incasso'),
 					self::STATE_PAID       => Mage::helper('sales')->__('Paid'),
 					self::STATE_CANCELED   => Mage::helper('sales')->__('Canceled'),
 			);
 		}
 		return self::$_states;
+	}
+	/**
+	 * Retrieve invoice state name by state identifier
+	 *
+	 * @param   int $stateId
+	 * @return  string
+	 */
+	public function getStateName($stateId = null)
+	{
+		 
+		if (is_null($stateId)) {
+			$stateId = $this->getState();
+		}
+	
+		if (is_null(self::$_states)) {
+			self::getStates();
+		}
+		if (isset(self::$_states[$stateId])) {
+			return self::$_states[$stateId];
+		}
+		return Mage::helper('sales')->__('Unknown State');
 	}
 
 	
