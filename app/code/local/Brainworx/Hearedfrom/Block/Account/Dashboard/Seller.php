8c09bc00b157b89763e39f6928d2e14ba2dee938
Brainworx <Brainworx@users.noreply.github.com>
2018-05-29 14:25:32 +0200

    Ticket 368: unieke link in myaccount

--- app/code/local/Brainworx/Hearedfrom/Block/Account/Dashboard/Seller.php
+++ app/code/local/Brainworx/Hearedfrom/Block/Account/Dashboard/Seller.php
@@ -31,7 +31,8 @@ class Brainworx_Hearedfrom_Block_Account_Dashboard_Seller extends Mage_Core_Bloc
      */
     public function isSeller()
     {        
-        return !empty($this->getSeller());
+        $showLink = Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('UNIQUE_LINK_FRONTEND')->getValue('text');
+        return $showLink && !empty($this->getSeller());
     }
     public function getSellerName(){
         $seller = $this->getSeller();
@@ -46,5 +47,9 @@ class Brainworx_Hearedfrom_Block_Account_Dashboard_Seller extends Mage_Core_Bloc
             return $seller['unique_link'];
         }
         return '';
+    }
+    public function getExtraSellerInfo(){
+         
+        return  Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('UNIQUE_LINK_EXTRA_INFO')->getValue('html');
     }
 }
