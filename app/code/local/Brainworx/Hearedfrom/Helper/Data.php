7cc467782a20c5b6d42e1951a4eebc51014d4bb3
Stijn <Stijn.heylen@brainworx.be>
2018-07-02 21:29:00 +0200

    Ticket 370: commit 1 nuttige documenten // doc_menu in myaccount

--- app/code/local/Brainworx/Hearedfrom/Helper/Data.php
+++ app/code/local/Brainworx/Hearedfrom/Helper/Data.php
@@ -1,4 +1,13 @@
 <?php
 class Brainworx_Hearedfrom_Helper_Data extends Mage_Checkout_Helper_Data
 {
+	public function getDocMenuBlockId(){
+		$customer = Mage::getSingleton('customer/session')->getCustomer();
+		if(!empty($customer)){
+			$salesforce = Mage::getModel('hearedfrom/salesForce')->loadByCustid($customer->getEntityId());
+			if(!empty($salesforce)){
+				return "doc_menu";
+			}
+		}
+	}
 }
