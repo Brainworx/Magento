e591601e016726f694e5924edf851148060bebbe
Brainworx <Brainworx@users.noreply.github.com>
2018-12-05 14:50:27 +0100

    Ticket 409: patient

---
+++ app/code/local/Brainworx/Hearedfrom/Model/Address/Abstract.php
@@ -0,0 +1,19 @@
+<?php
+/**
+
+/**
+ * Address abstract model
+ *
+ * @category   Mage
+ * @package    Mage_Customer
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Brainworx_Hearedfrom_Model_Address_Abstract extends Mage_Customer_Model_Address_Abstract
+{
+    /**
+     * Possible customer address types
+     */
+    const TYPE_PATIENT  = 'patient';
+
+   
+}
