d94b29cd58d89edc395b311ebdcad56170f5ad14
Brainworx <Brainworx@users.noreply.github.com>
2019-06-17 15:26:55 +0200

    Ticket 484: ristorno crediteren

--- app/code/local/Brainworx/Hearedfrom/Model/Resource/SalesCommission.php
+++ app/code/local/Brainworx/Hearedfrom/Model/Resource/SalesCommission.php
@@ -7,40 +7,20 @@ class Brainworx_Hearedfrom_Model_Resource_SalesCommission extends Mage_Core_Mode
         $this->_init('hearedfrom/salesCommission', 'entity_id');
     }
     /**
-     * Load foreign key related data to include in the model when selected
-     * (showed in admin form)
-     * (non-PHPdoc)
-     * @see Mage_Core_Model_Resource_Db_Abstract::_getLoadSelect()
+     * Optional method to load 1 entity by parameters
+     * @param int $id
+     * @return multitype:salescommission  array of salescommission data with column as keys
      */
-//     protected function _getLoadSelect($field, $value, $object)
-//     {
-//     	$select = parent::_getLoadSelect($field, $value, $object);
-    	
-//     	$resource = Mage::getSingleton('core/resource');
+    public function loadByLastCommission($order_id,$item_id)
+    {
+    	$adapter = $this->_getReadAdapter();
     
-    	//TODO update
-//     	$select->join(
-// 					array('order' => $resource->getTableName('sales/order')),
-// 					'orig_order_id = order.entity_id',
-// 					array('customer_id','increment_id',
-// 							'customer'=>'concat(customer_firstname," ",customer_lastname)'
-// 					)
-// 		);
-//     	$select->join(
-//     			array('baddress' => $resource->getTableName('sales_flat_order_address')),
-//     			'billing_address_id = baddress.entity_id',
-//     			array('billing_address' => 'concat(baddress.street,", ",baddress.city," ", baddress.postcode)' )
-//     	);
-//     	$select->join(
-//     			array('saddress' => $resource->getTableName('sales_flat_order_address')),
-//     			'shipping_address_id = saddress.entity_id',
-//     			array('shipping_address' => 'concat(saddress.street,", ",saddress.city," ", saddress.postcode)' )
-//     	);
-//         $select->join(
-// 	        		array('item' => $resource->getTableName('sales/order_item')),
-// 	        		'order_item_id = item.item_id',
-// 	        		array('product' => 'name', 'sku') //select name as product
-//         );
-//     	return $select;
-//     }
+    	$select = $adapter->select()
+    	->from($this->getMainTable())
+    	->where($this->getMainTable().'.'.'orig_order_id'.'=?',$order_id)
+    	->where($this->getMainTable().'.'.'order_item_id'.'=?',$item_id)
+        ->order($this->getMainTable().'.'.'entity_id DESC');
+    
+    	return $adapter->fetchRow($select);
+    }
 }
