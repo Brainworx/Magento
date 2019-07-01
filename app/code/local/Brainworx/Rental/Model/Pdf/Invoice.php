3163957a19920ff27bd24f673f09f7cd778c9352
Brainworx <Brainworx@users.noreply.github.com>
2019-07-01 12:37:49 +0200

    Ticket 488: footer invoice

--- app/code/local/Brainworx/Rental/Model/Pdf/Invoice.php
+++ app/code/local/Brainworx/Rental/Model/Pdf/Invoice.php
@@ -69,7 +69,7 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 			$order = $invoice->getOrder();
 			$orderComment = null;
 			if(!empty($order->getPatientName()))
-				$orderComment ='Patiëntgegevens'.': '. $order->getPatientFirstname().' '.$order->getPatientName() . "<br>";
+				$orderComment ='PatiÃ«ntgegevens'.': '. $order->getPatientFirstname().' '.$order->getPatientName() . "<br>";
 			if(!empty($order->getPatientBirthDate())){
 				$birthdatetext = Mage::helper('sales')->__('BirthDate Patient:').Mage::helper('core')->formatDate($order->getPatientBirthDate(), 'medium', false);
 				$orderComment = $orderComment.$birthdatetext.'<br>';
@@ -283,48 +283,9 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 		
 		// write first column second block********************************************************
 		//init
-		$flineBlock = array(
-				'lines'  => array(),
-				'height' => 15
-		);
-		$linesContent = array();
-		//end init
-		$linesContent[]=$name;
-		$linesContent[]=Mage::getStoreConfig('general/store_information/address');
-		$linesContent[]='BTW '.Mage::getStoreConfig('general/store_information/merchant_vat_number');
-		//write first column second block *********************************************
-		foreach($linesContent as $c){
-			$flineBlock['lines'][] = array(array('text'      => $c,
-					'feed'      => 100,
-					'align'     => 'left')
-			);
-		}
-		$this->y =75;//Draw in footer space
-		$page = $this->drawLineBlocks($page, array($flineBlock));
 		
-		// Column 2 second block *******************************************************
-		//init
-		$this->y += 65; //reset from previous write 
+		$this->insertFooterLogo($page);
 		
-		$flineBlock = array(
-				'lines'  => array(),
-				'height' => 15
-		);
-		$linesContent = array();
-		//end init
-		$linesContent[] = Mage::getStoreConfig('general/store_information/phone');
-		$linesContent[] = Mage::getStoreConfig('trans_email/ident_general/email');
-		$linesContent[] = Mage::getStoreConfig('web/secure/base_url');
-		
-		//write
-		foreach($linesContent as $c){
-			$flineBlock['lines'][] = array(array('text'      => $c,
-					'feed'      => 350,
-					'align'     => 'left')
-			);
-		}
-		$this->y -= 20;
-		$page = $this->drawLineBlocks($page, array($flineBlock));
 		//end footnote
 		
 	}
@@ -469,119 +430,6 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 	
 			$addressesEndY = min($addressesEndY, $this->y);
 			$this->y = $addressesEndY;
-	
-// 			$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
-// 			$page->setLineWidth(0.5);
-// 			$page->drawRectangle(25, $this->y, 275, $this->y-25);
-// 			$page->drawRectangle(275, $this->y, 570, $this->y-25);
-	
-// 			$this->y -= 15;
-// 			$this->_setFontBold($page, 12);
-// 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-// 			$page->drawText(Mage::helper('sales')->__('Payment Method'), 35, $this->y, 'UTF-8');
-// 			$page->drawText(Mage::helper('sales')->__('Shipping Method:'), 285, $this->y , 'UTF-8');
-	
-// 			$this->y -=10;
-// 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
-	
-// 			$this->_setFontRegular($page, 10);
-// 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-	
-// 			$paymentLeft = 35;
-// 			$yPayments   = $this->y - 15;
-// 		}
-// 		else {
-// 			$yPayments   = $addressesStartY;
-// 			$paymentLeft = 285;
-// 		}
-	
-// 		foreach ($payment as $value){
-// 			if (trim($value) != '') {
-// 				//Printing "Payment Method" lines
-// 				$value = preg_replace('/<br[^>]*>/i', "\n", $value);
-// 				foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
-// 					$page->drawText(strip_tags(trim($_value)), $paymentLeft, $yPayments, 'UTF-8');
-// 					$yPayments -= 15;
-// 				}
-// 			}
-// 		}
-	
-// 		if ($order->getIsVirtual()) {
-// 			// replacement of Shipments-Payments rectangle block
-// 			$yPayments = min($addressesEndY, $yPayments);
-// 			$page->drawLine(25,  ($top - 25), 25,  $yPayments);
-// 			$page->drawLine(570, ($top - 25), 570, $yPayments);
-// 			$page->drawLine(25,  $yPayments,  570, $yPayments);
-	
-// 			$this->y = $yPayments - 15;
-// 		} else {
-// 			$topMargin    = 15;
-// 			$methodStartY = $this->y;
-// 			$this->y     -= 15;
-	
-// 			foreach (Mage::helper('core/string')->str_split($shippingMethod, 45, true, true) as $_value) {
-// 				$page->drawText(strip_tags(trim($_value)), 285, $this->y, 'UTF-8');
-// 				$this->y -= 15;
-// 			}
-	
-// 			$yShipments = $this->y;
-// 			$totalShippingChargesText = "(" . Mage::helper('sales')->__('Total Shipping Charges') . " "
-// 					. $order->formatPriceTxt($order->getShippingAmount()) . ")";
-	
-// 			$page->drawText($totalShippingChargesText, 285, $yShipments - $topMargin, 'UTF-8');
-// 			$yShipments -= $topMargin + 10;
-	
-// 			$tracks = array();
-// 			if ($shipment) {
-// 				$tracks = $shipment->getAllTracks();
-// 			}
-// 			if (count($tracks)) {
-// 				$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
-// 				$page->setLineWidth(0.5);
-// 				$page->drawRectangle(285, $yShipments, 510, $yShipments - 10);
-// 				$page->drawLine(400, $yShipments, 400, $yShipments - 10);
-// 				//$page->drawLine(510, $yShipments, 510, $yShipments - 10);
-	
-// 				$this->_setFontRegular($page, 9);
-// 				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-// 				//$page->drawText(Mage::helper('sales')->__('Carrier'), 290, $yShipments - 7 , 'UTF-8');
-// 				$page->drawText(Mage::helper('sales')->__('Title'), 290, $yShipments - 7, 'UTF-8');
-// 				$page->drawText(Mage::helper('sales')->__('Number'), 410, $yShipments - 7, 'UTF-8');
-	
-// 				$yShipments -= 20;
-// 				$this->_setFontRegular($page, 8);
-// 				foreach ($tracks as $track) {
-	
-// 					$CarrierCode = $track->getCarrierCode();
-// 					if ($CarrierCode != 'custom') {
-// 						$carrier = Mage::getSingleton('shipping/config')->getCarrierInstance($CarrierCode);
-// 						$carrierTitle = $carrier->getConfigData('title');
-// 					} else {
-// 						$carrierTitle = Mage::helper('sales')->__('Custom Value');
-// 					}
-	
-// 					//$truncatedCarrierTitle = substr($carrierTitle, 0, 35) . (strlen($carrierTitle) > 35 ? '...' : '');
-// 					$maxTitleLen = 45;
-// 					$endOfTitle = strlen($track->getTitle()) > $maxTitleLen ? '...' : '';
-// 					$truncatedTitle = substr($track->getTitle(), 0, $maxTitleLen) . $endOfTitle;
-// 					//$page->drawText($truncatedCarrierTitle, 285, $yShipments , 'UTF-8');
-// 					$page->drawText($truncatedTitle, 292, $yShipments , 'UTF-8');
-// 					$page->drawText($track->getNumber(), 410, $yShipments , 'UTF-8');
-// 					$yShipments -= $topMargin - 5;
-// 				}
-// 			} else {
-// 				$yShipments -= $topMargin - 5;
-// 			}
-	
-// 			$currentY = min($yPayments, $yShipments);
-	
-			// replacement of Shipments-Payments rectangle block
-// 			$page->drawLine(25,  $methodStartY, 25,  $currentY); //left
-// 			$page->drawLine(25,  $currentY,     570, $currentY); //bottom
-// 			$page->drawLine(570, $currentY,     570, $methodStartY); //right
-	
-// 			$this->y = $currentY;
-// 			$this->y -= 15;
 		}
 	}
 	/**
@@ -690,6 +538,45 @@ class Brainworx_Rental_Model_Pdf_Invoice extends Mage_Sales_Model_Order_Pdf_Invo
 		$this->drawLineBlocks($page, array($lineBlock), array('table_header' => true));
 		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
 		$this->y -= 20;
+	}
+	/**
+	 * Insert footer logo to pdf page
+	 *
+	 * @param Zend_Pdf_Page $page
+	 * @param null $store
+	 */
+	protected function insertFooterLogo(&$page)
+	{
+		
+		$image = Mage::getBaseDir('media') . '/sales/store/logo/footer_invoice_zorgpunt.png';
+		if (is_file($image)) {
+			$image       = Zend_Pdf_Image::imageWithPath($image);
+			$top         = 90; //bottom border of the page
+			$widthLimit  = 494; //SHE update so logo is smaller and invoice address get higher
+			$heightLimit = 36; //SHE update so logo is smaller and invoice address get higher
+			$width       = $image->getPixelWidth();
+			$height      = $image->getPixelHeight();
+
+			//preserving aspect ratio (proportions)
+			$ratio = $width / $height;
+			if ($ratio > 1 && $width > $widthLimit) {
+				$width  = $widthLimit;
+				$height = $width / $ratio;
+			} elseif ($ratio < 1 && $height > $heightLimit) {
+				$height = $heightLimit;
+				$width  = $height * $ratio;
+			} elseif ($ratio == 1 && $height > $heightLimit) {
+				$height = $heightLimit;
+				$width  = $widthLimit;
+			}
+			$y1 = $top - $height;
+			$y2 = $top;
+			$x1 = 50;
+			$x2 = $x1 + $width;
+
+			//coordinates after transformation are rounded by Zend
+			$page->drawImage($image, $x1, $y1, $x2, $y2);
+		}
 	}
 	
 }
